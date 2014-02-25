f8b209261349d29c845f0a38d9f387dcdf7e7dda
Adam Okoń <adam@okon.eu>
2014-02-25 22:07:13 +0100

    Added start page for app

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe HomeController do
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get :index
+      response.should be_success
+    end
+
+    it "renders index template" do
+      get :index
+      response.should render_template(:index)
+    end
+  end
+
+end
